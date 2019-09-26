38849cf92dd5bb1f00ff014568a0e0a490e7f93a
waterylime46@aol.com <waterylime46@aol.com>
2019-09-26 12:41:20 -0600

    project completed tutor sesion to fix Java

--- script.js
+++ script.js
@@ -3,16 +3,18 @@
 function genarate(){
 // set pasword length/complexity
 let complexity = document.getElementById("slider").value;
-
+console.log(complexity)
 
 //all password values
 let values = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz!@#$%^&*?=+_()";
 
 let password= "";
 
+let randomValue = Math.floor(Math.random()* values.length)
+
 //creat for loop for password
 for(var i = 0; i <= complexity; i++){
-    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.lenght)));
+    password = password + values.charAt(Math.floor(Math.random() * Math.floor(values.length)));
 
 }
 //add pasword to text box
@@ -22,6 +24,21 @@ document.getElementById("display").value = password;
 document.getElementById("lastPassword").innerHTML += password + "<br />";
 
 
+} 
+document.getElementById("generate").onclick = function(){
+    genarate()
 }
 
+
+//copy password to clipboard
+function copyPassword(){
+    document.getElementById("display").select();
+
+  
+    alert(document.getElementById("display").value)
+}
+
+document.getElementById("copy").onclick = function(){
+    copyPassword()
+}
 
